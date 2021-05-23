ec04a0f87b49c6c2994809b6ca5323894f91ef29
Li.Jie <HelloBox@outlook.com>
2021-05-23 21:23:48 +0800

    update

---
+++ src/test/kotlin/test.kt
@@ -0,0 +1,11 @@
+/**
+ * @Author Lijie
+ * @date  2021/5/23:21:08
+ */
+class Mains {
+    companion object {
+        fun main(args: Array<String>) {
+            print("test")
+        }
+    }
+}
